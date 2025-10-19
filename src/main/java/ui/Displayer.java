59b5079861e4ca285fa338b1e4db99b23ae212e7
Favkes <tho.strongest.avenger@gmail.com>
2025-10-19 22:28:02 +0200

    Implement frameBodyPrevious field along with .goToPixel() and .renderFrame() methods, in efforts of constructing a more optimal rendering algorithm

--- src/main/java/ui/Displayer.java
+++ src/main/java/ui/Displayer.java
@@ -2,12 +2,35 @@ package ui;
 
 
 public class Displayer extends DisplayCore {
+    public String frameBodyPrevious;
 
     public Displayer() throws Exception {
         super();
 
         frameBody = Color.Background.BLUE + Color.Foreground.BLUE
                 + ".".repeat(frameBodySize) + Color.RESET;
+    }
+
+    private void goToPixel(int y, int x) {
+        // ANSI codes use 1-based indexing, thus the off-by-1-shift
+        terminal.writer().print(String.format("\u001B[%d;%dH", y + 1, x + 1));
+    }
+
+    private void renderFrame() {
+        String frameBodyLatest = frameBody;
+        boolean continuousEscapeCodeSequence = false;
+        String precedingCharFormatting;
+
+        for (int i=0; i<frameBodySize; i++) {
+            if (frameBodyPrevious.charAt(i) != frameBody.charAt(i)) {
+                if (continuousEscapeCodeSequence) {
+                    continuousEscapeCodeSequence = true;
+                }
+            }
+
+        }
+
+        frameBodyPrevious = frameBodyLatest;
     }
 
     @Override
