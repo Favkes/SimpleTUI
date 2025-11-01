288f60669d946a33314a91a349b8fac64ede87cb
Favkes <tho.strongest.avenger@gmail.com>
2025-11-01 20:45:00 +0100

    Implement simple Texture class object for testing.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import org.fusesource.jansi.AnsiConsole;
+import ui.Color;
 import ui.Displayer;
+import components.Texture;
 
 
 public class Main {
@@ -10,9 +12,21 @@ public class Main {
         try (Displayer display = new Displayer()) {
             // App init
             display.init();
+            Texture texture = new Texture(
+                    Color.Background.RED + "@" + Color.Background.BLUE + "#"
+            );
 
             // Main app loop
             display.refreshDisplay();
+
+            texture.loadTexture(
+                    "0123" + Color.Background.BLUE + "4567" + Color.Foreground.RED + "89ab"
+            );
+            System.out.print("\n");
+            texture.test();
+            System.out.print("\n");
+            for (int i=0; i<10; i++)
+                System.out.println(String.format("%d %s" + Color.RESET + "\n", i, texture.fetchChunk(0, i)));
 
         }
         catch (Exception e) {
