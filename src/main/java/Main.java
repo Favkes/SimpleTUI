66c8426a628fb3fa8f7ecd0d6dfa508937899dad
Favkes <tho.strongest.avenger@gmail.com>
2025-11-30 17:33:13 +0100

    Create fundamental Frame class structure, implement in Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import org.fusesource.jansi.AnsiConsole;
 import ui.Color;
 import ui.Displayer;
 import components.Texture;
+import components.Frame;
+
 
 
 public class Main {
@@ -10,6 +12,7 @@ public class Main {
         AnsiConsole.systemInstall();
 
         try (Displayer display = new Displayer()) {
+
             // App init
             display.init();
             Texture texture = new Texture(
@@ -23,21 +26,19 @@ public class Main {
                     + Color.generateRGB(false, 70, 70, 70)
                     + "."
                     + Color.generateRGB(true, 100, 250, 100)
-                    + Color.generateRGB(false, 100, 100, 100)
+                    + Color.generateRGB(false, 100, 250, 100)
                     + "."
                     + Color.generateRGB(true, 130, 130, 130)
                     + Color.generateRGB(false, 130, 130, 130)
                     + "."
             );
-//            texture.loadTexture(
-//                    Color.Background.BLUE + "0123" + Color.generateRGB(true, 250, 170, 170) + Color.Foreground.BLACK + "4567" + Color.Foreground.RED + "89ab"
-//            );
             System.out.print("\n");
             texture.preGenerate(50);
 
-
             for (int i=0; i<10; i++)
                 System.out.printf("%s%s\n", texture.fetchChunk(i, i+20), Color.RESET);
+
+            Frame frame1 = new Frame();
 
             // Main app loop
 //            display.refreshDisplay();
---
+++ src/main/java/components/Frame.java
@@ -0,0 +1,15 @@
+package components;
+
+public class Frame {
+    public int[] position;
+    public Texture texture;
+    public int width;
+    public int height;
+
+    public Frame() {
+        position = new int[]{0, 0};
+        texture = null;
+        width = 0;
+        height = 0;
+    }
+}
