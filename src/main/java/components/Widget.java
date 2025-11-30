ea4bf327695b267ba4d8eb9d7a03fd7272f1c798
Favkes <tho.strongest.avenger@gmail.com>
2025-11-30 17:51:55 +0100

    Create abstract Widget class as parent for all widget classes, implementing a few overloaded class object constructors

---
+++ src/main/java/components/Widget.java
@@ -0,0 +1,32 @@
+package components;
+
+public abstract class Widget {
+    public int x;
+    public int y;
+    public int width;
+    public int height;
+    public Texture texture;
+
+    public Widget(int posY, int posX, int height, int width, Texture texture) {
+        x = 0;
+        y = 0;
+        this.width = 0;
+        this.height = 0;
+        this.texture = texture;
+    }
+
+    public Widget(int posY, int posX, int height, int width, String textureSeed) {
+        this(posY, posX, height, width, new Texture(textureSeed));
+    }
+
+    public Widget(int posY, int posX, int height, int width) {
+        this(posY, posX, height, width, "DefaultTexture_");
+    }
+
+    public Widget() {
+        this(0, 0, 0, 0);
+
+    }
+
+    public abstract void printInfo();
+}
