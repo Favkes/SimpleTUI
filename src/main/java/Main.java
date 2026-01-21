17c84247a7c719db2f1847af96e97df1f0daae45
Favkes <tho.strongest.avenger@gmail.com>
2026-01-21 10:24:42 +0100

    Create more advanced app structure implementing all currently existing resources, remove unnecessary AnsiConsole import and calls.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import components.*;
-import org.fusesource.jansi.AnsiConsole;
+//import org.fusesource.jansi.AnsiConsole;
+//import org.jline.utils.InfoCmp;
 import ui.Color;
 import ui.Displayer;
 //import ui.InputManager;
@@ -12,7 +13,7 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         // ANSI support install
-        AnsiConsole.systemInstall();
+//        AnsiConsole.systemInstall();
         WindowManager manager = new WindowManager();
         AdvancedTexture displayBackgroundTexture = new AdvancedTexture(
                 Color.generateRGB(false, 30, 30, 30)
@@ -50,24 +51,10 @@ public class Main {
             System.out.print("\n");
             texture.test();
 
-            AdvancedTexture texture2 = new AdvancedTexture(
-                    Color.generateRGB(true, 60, 60, 60)
-                            + Color.generateRGB(false,  190, 160, 190)
-                            + "["
-                            + Color.generateRGB(true, 150, 200, 150)
-                            + Color.generateRGB(false, 30, 30, 30)
-                            + "+++"
-                            + Color.generateRGB(true, 60, 60, 60)
-                            + Color.generateRGB(false, 190, 160, 190)
-                            + "]",
-                    1,
-                    r -> 0
-            );
-
             for (int i=0; i<10; i++)
                 System.out.printf(
                         "%s%s\n",
-                        texture.generateRepeatingSubarray(i, i+20).stream()
+                        texture.generateRepeatingSubarray(0, 20, i).stream()
                                 .map(Pixel::toString)
                                 .collect(Collectors.joining()),
                         Color.RESET);
@@ -77,27 +64,119 @@ public class Main {
             );
             display.windowManager.contents.add(frame1);
 
-            Frame frame2 = new Frame(
-                    display.windowManager.root, 25, 15, 7, 5, texture2
-            );
-            display.windowManager.contents.add(frame2);
 
-            display.renderComponentOfIndex(0);
-            display.renderComponentOfIndex(1);
+            // WINDOW BORDERS
+            AdvancedTexture windowBorderTexture = new AdvancedTexture(
+                    Color.generateRGB(true, 60, 60, 60)
+                            + Color.generateRGB(false,  160, 250, 160)
+                            + "|",
+                    1,
+                    r -> 0
+            );
+            AdvancedTexture windowBorderTexture2 = new AdvancedTexture(
+                    Color.generateRGB(true, 60, 60, 60)
+                            + Color.generateRGB(false,  160, 250, 160)
+                            + "-",
+                    1,
+                    r -> 0
+            );
+            Frame windowBorderLeft = new Frame(
+                    display.windowManager.root, 0, 0, display.terminal.getHeight(), 1, windowBorderTexture
+            );
+            display.windowManager.contents.add(windowBorderLeft);
+            Frame windowBorderRight = new Frame(
+                    display.windowManager.root, 0, display.terminal.getWidth()-1, display.terminal.getHeight(), 1, windowBorderTexture
+            );
+            display.windowManager.contents.add(windowBorderRight);
+            Frame windowBorderTop = new Frame(
+                    display.windowManager.root, 0, 0, 1, display.terminal.getWidth(), windowBorderTexture2
+            );
+            display.windowManager.contents.add(windowBorderTop);
+            Frame windowBorderBottom = new Frame(
+                    display.windowManager.root, display.terminal.getHeight()-1, 0, 1, display.terminal.getWidth(), windowBorderTexture2
+            );
+            display.windowManager.contents.add(windowBorderBottom);
+
+            // vertical line between menu and chat
+            display.windowManager.contents.add(
+                    new Frame(
+                            display.windowManager.root,
+                            1, 25, display.terminal.getHeight()-2, 1,
+                            windowBorderTexture
+                    )
+            );
+
+            // FRAMES
+//            display.windowManager.contents.add(
+//                    new Frame(
+//                            display.windowManager.root,
+//                            2, 3, 4, 6,
+//                            texture
+//                    )
+//            );
+            Frame menuAFrame = new Frame(
+                    display.windowManager.root,
+                    2, 3, 4, 6,
+                    texture
+            ); display.windowManager.contents.add(menuAFrame);
+            Text menuAText = new Text(
+                    menuAFrame,
+                    0, 0,
+                    "Menu A"
+            ); display.windowManager.contents.add(menuAText);
+
+            AdvancedTexture messageWriteBoxTexture = new AdvancedTexture(
+                    Color.generateBgFg(200, 200, 200, 30, 30, 80)
+                    + " "
+            );
+            Frame messageWriteBox = new Frame(
+                    display.windowManager.root,
+                    display.terminal.getHeight()-2-1,
+                    26,
+                    2,
+                    display.terminal.getWidth()-26-1,
+                    messageWriteBoxTexture
+            );
+            display.windowManager.contents.add(messageWriteBox);
+
+            Text messageWriteBoxText = new Text(
+                    messageWriteBox,
+                    display.terminal.getHeight()-2-1,
+                    26,
+                    ""
+            );
+            display.windowManager.contents.add(messageWriteBoxText);
+            messageWriteBoxText.updateContentLength();
+            messageWriteBoxText.renderToPixels();
 
 
             // Input thread setup
-            display.inputManager.bindKey('q', () -> display.running.set(false));
-            display.inputManager.bindKey('w', () -> frame1.y--);
-            display.inputManager.bindKey('a', () -> frame1.x--);
-            display.inputManager.bindKey('s', () -> frame1.y++);
-            display.inputManager.bindKey('d', () -> frame1.x++);
+            display.inputManager.bindKey("\033", () -> display.running.set(false)); // escape
+            display.inputManager.bindKey("\033[A", () -> frame1.y--);
+            display.inputManager.bindKey("\033[D", () -> frame1.x--);
+            display.inputManager.bindKey("\033[B", () -> frame1.y++);
+            display.inputManager.bindKey("\033[C", () -> frame1.x++);
+
+            display.inputManager.bindKey("\r", () -> messageWriteBoxText.updateContent(""));    // enter
+            display.inputManager.bindKey("\177", messageWriteBoxText::removeLast);  // backspace
+
+            // adding all the valid ASCII characters:
+            for (char c = 32; c <= 126; c++) {
+                char keyChar = c;
+                display.inputManager.bindKey(
+                        String.valueOf(keyChar),
+                        () -> messageWriteBoxText.updateContent(messageWriteBoxText.content + keyChar)
+                );
+            }
+
 
             while (display.running.get()) {
                 display.generateBlankPixelMatrix();
                 display.rebuildEmpty();
-                display.renderComponentOfIndex(0);
-                display.renderComponentOfIndex(1);
+                for (int i=0; i<display.windowManager.contents.size(); i++)
+                    display.renderComponentOfIndex(i);
+//                display.renderComponentOfIndex(0);
+//                display.renderComponentOfIndex(1);
                 display.refreshDisplay();
 //                frame1.y += 1;
                 Thread.sleep(1000 / 20);
@@ -108,6 +187,6 @@ public class Main {
         }
 
         // ANSI support uninstall
-        AnsiConsole.systemUninstall();
+//        AnsiConsole.systemUninstall();
     }
 }
